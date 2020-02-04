a49cf21ce612314ea4430eb2f42a6b3aeb212c69
Jaji <aniljturan@gmail.com>
2020-02-04 13:51:25 +0300

    worker profile vc done

--- StepJob/Scenes/EmployeeMain/MyProfileViewController.swift
+++ StepJob/Scenes/EmployeeMain/MyProfileViewController.swift
@@ -18,10 +18,19 @@ class MyProfileViewController: UIViewController {
     @IBOutlet weak var confirmButton: UIButton!
     @IBOutlet weak var callButton: UIButton!
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobCountLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var genderAgeLabel: UILabel!
+    
+    var oldJobs: [Job]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setInterface()
+        getUser()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -38,6 +47,26 @@ class MyProfileViewController: UIViewController {
         messageButton.isHidden = true
         confirmButton.isHidden = true
         callButton.isHidden = true
+        
+        nameLabel.text = workerUser?.fullName
+        jobCountLabel.text = String(workerUser?.jobs?.count ?? 0)
+        ratingLabel.text = String(Double(workerUser?.rating ?? 5))
+        locationLabel.text = workerUser?.location
+        genderAgeLabel.text = (workerUser?.gender ?? "Belirtilmemiş") + ", " + String(workerUser?.age ?? 0)
+        bioLabel.text = workerUser?.about
+        
+    }
+    
+    func getUser() {
+        
+        WebService().workerMe { [weak self] (response, error) in
+            guard let strongSelf = self else { return }
+            if !error, let worker = Mapper<Worker>().map(JSONObject: response) {
+                workerUser = worker
+                strongSelf.setInterface()
+                strongSelf.oldJobs = worker.jobs
+            }
+        }
     }
     
     func setupForEmployer() {
@@ -47,16 +76,21 @@ class MyProfileViewController: UIViewController {
         confirmButton.layer.cornerRadius = 25.0
         callButton.layer.cornerRadius = 25.0
     }
+    
 }
 
 extension MyProfileViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return oldJobs?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DashboardJobTVC", for: indexPath) as! DashboardJobTVC
-        cell.fillCell(job: "Yazılım Geliştirme Elemanı", company: "ROTA İNTERNET TEKNOLOJİ HİZMETLERİ", location: "İstanbul(Asya)", price: "100₺")
+        if let job = oldJobs?[indexPath.row] {
+            cell.fillCell(with: job)
+        } else {
+            cell.fillCell(job: "Yazılım Geliştirme Elemanı", company: "ROTA İNTERNET TEKNOLOJİ HİZMETLERİ", location: "İstanbul(Asya)", price: "100₺")
+        }
         return cell
     }
     
